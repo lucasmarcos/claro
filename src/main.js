28f8cfc29ec478d05e34402e9da17c1829c22c9a
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-09-10 20:51:41 -0300

    burro

--- src/main.js
+++ src/main.js
@@ -18,8 +18,6 @@ BEGIN
 END
 `;
 
-let graph = new Map();
-
 /*
 bloco seq:
   p1
@@ -34,26 +32,53 @@ bloco seq:
   p7
 */
 
+const callStack = [];
+
 const dfs = (node) => {
   if (node.type === "call") {
-    const label = node.child(0).text;
-    graph.set(label, new Set());
+    callStack.push(node.child(0).text);
   } else if (node.type === "begin") {
+    callStack.push("(");
   } else if (node.type === "parbegin") {
+    callStack.push("[");
   } else if (node.type === "end") {
+    callStack.push(")");
   } else if (node.type === "parend") {
+    callStack.push("]");
   }
 
-  const count = node.childCount;
+  if (node.childCount === 0) return;
 
-  if (count === 0) return;
-
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < node.childCount; i++) {
     dfs(node.child(i));
   }
 };
 
+const process = () => {
+  const tree = [];
+
+  for (const statement of callStack) {
+    if (statement === "(") {
+      if (tree.length > 0) {
+        tree[tree.length - 1].push([]);
+      } else {
+        tree.push([]);
+      }
+    } else if (statement === ")") {
+      tree.push([]);
+    } else if (statement === "[") {
+    } else if (statement === "]") {
+    } else {
+      tree[tree.length - 1].push(statement);
+    }
+  }
+
+  console.log(tree);
+};
+
 const dot = () => {
+  const graph = new Map();
+
   console.log(graph);
 
   let out = "digraph {";
@@ -61,8 +86,8 @@ const dot = () => {
   for (const edge of graph) {
     const src = edge[0];
     for (const dst of edge[1]) {
-	out = `${out}
-${src} -> ${dst};`
+      out = `${out}
+${src} -> ${dst};`;
     }
   }
 
@@ -74,11 +99,15 @@ ${src} -> ${dst};`
 
 const main = async () => {
   await Parser.init();
-  const parbeginParendLangauge = await Parser.Language.load("tree-sitter-parbeginparend.wasm");
+  const parbeginParendLangauge = await Parser.Language.load(
+    "tree-sitter-parbeginparend.wasm",
+  );
   const parser = new Parser();
   parser.setLanguage(parbeginParendLangauge);
   const tree = parser.parse(example);
   dfs(tree.rootNode);
+  console.log(callStack);
+  process();
   console.log(dot());
 };
 
